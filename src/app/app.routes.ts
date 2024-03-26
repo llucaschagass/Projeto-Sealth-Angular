f5e38d234b1ee2abe8b9e2234e64e97078e1a41a
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-03-25 21:10:09 -0300

    feat: criando info component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
+import { InfoComponent } from './pages/info/info.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: "home",
         component: HomeComponent
+    },
+    {
+        path: "info",
+        component: InfoComponent
     }
 ];
