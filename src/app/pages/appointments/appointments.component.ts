6624f87cbb73892d78f86244b90dacf0d042c1db
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-05-13 15:51:33 -0300

    feat: criando a logica dos botoes de atualizar criar e deletar consultas

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -58,7 +58,6 @@ export class AppointmentsComponent implements OnInit {
   getAllAppointments(): void {
     this.appointmentsService.getAllAppointments().subscribe(
       appointments => {
-        console.log('Agendamentos obtidos:', appointments);
         this.appointments = appointments;
       },
       error => {
@@ -69,5 +68,178 @@ export class AppointmentsComponent implements OnInit {
 
   selectAppointment(appointment: any): void {
     this.selectedAppointment = appointment;
+  }
+
+  cancelAppointment(): void {
+    if (this.selectedAppointment) {
+      Swal.fire({
+        title: 'Cancelar Consulta',
+        text: 'Tem certeza que deseja cancelar a consulta selecionada?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#046B46',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Não'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.appointmentsService.cancelAppointment(this.selectedAppointment.id).subscribe(
+            () => {
+              console.log('Consulta cancelada:', this.selectedAppointment);
+              this.getAllAppointments();
+              Swal.fire({
+                title: 'Sucesso',
+                text: 'Consulta cancelada com sucesso!',
+                icon: 'success',
+                confirmButtonColor: '#046B46',
+                confirmButtonText: 'Ok',
+              });
+            },
+            error => {
+              console.error('Erro ao cancelar consulta:', error);
+              Swal.fire({
+                title: 'Erro',
+                text: 'Ocorreu um erro ao cancelar a consulta.',
+                icon: 'error',
+                confirmButtonColor: '#046B46',
+                confirmButtonText: 'OK'
+              });
+            }
+          );
+        }
+      });
+    } else {
+      Swal.fire({
+        title: 'Nenhuma Consulta Selecionada',
+        text: 'Favor selecionar uma consulta antes de cancelar.',
+        icon: 'error',
+        confirmButtonColor: '#046B46',
+        confirmButtonText: 'OK'
+      });
+    }
+  }
+
+  updateAppointment(): void {
+    if (this.selectedAppointment) {
+      Swal.fire({
+        title: 'Atualizar Consulta',
+        html: `
+          <input id="doctor" class="swal2-input" placeholder="Médico" value="${this.selectedAppointment.doctor}">
+          <input id="specialty" class="swal2-input" placeholder="Especialidade" value="${this.selectedAppointment.specialty}">
+          <input id="date" class="swal2-input" placeholder="Data" value="${this.selectedAppointment.date}">
+          <input id="location" class="swal2-input" placeholder="Local" value="${this.selectedAppointment.location}">
+          <input id="extraInfo" class="swal2-input" placeholder="Informações Extras" value="${this.selectedAppointment.extraInfo}">
+        `,
+        showCancelButton: true,
+        confirmButtonColor: '#046B46',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar',
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          const doctor = (document.getElementById('doctor') as HTMLInputElement).value;
+          const specialty = (document.getElementById('specialty') as HTMLInputElement).value;
+          const date = (document.getElementById('date') as HTMLInputElement).value;
+          const location = (document.getElementById('location') as HTMLInputElement).value;
+          const extraInfo = (document.getElementById('extraInfo') as HTMLInputElement).value;
+  
+          const updatedData = {
+            doctor,
+            specialty,
+            date,
+            location,
+            extraInformation: extraInfo
+          };
+  
+          return this.appointmentsService.updateAppointment(this.selectedAppointment.id, updatedData).toPromise();
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          console.log('Consulta atualizada com sucesso!');
+          this.getAllAppointments();
+          Swal.fire({
+            title: 'Sucesso',
+            text: 'Consulta atualizada com sucesso!',
+            icon: 'success',
+            confirmButtonColor: '#046B46',
+            confirmButtonText: 'Ok',
+          });
+        }
+      }).catch((error) => {
+        console.error('Erro ao atualizar consulta:', error);
+        Swal.fire({
+          title: 'Erro',
+          text: 'Erro ao atualizar consulta. Por favor, tente novamente.',
+          icon: 'error',
+          confirmButtonColor: '#046B46',
+          confirmButtonText: 'Ok',
+        });
+      });
+    } else {
+      Swal.fire({
+        title: 'Nenhuma Consulta Selecionada',
+        text: 'Favor selecionar uma consulta antes de atualizar.',
+        icon: 'error',
+        confirmButtonColor: '#046B46',
+        confirmButtonText: 'OK'
+      });
+    }
+  }
+  
+  createAppointment(): void {
+    Swal.fire({
+      title: 'Nova Consulta',
+      html: `
+        <input id="doctor" class="swal2-input" placeholder="Médico">
+        <input id="specialty" class="swal2-input" placeholder="Especialidade">
+        <input id="date" class="swal2-input" placeholder="Data">
+        <input id="location" class="swal2-input" placeholder="Local">
+        <input id="extraInfo" class="swal2-input" placeholder="Informações Extras">
+      `,
+      showCancelButton: true,
+      confirmButtonColor: '#046B46',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        const doctor = (document.getElementById('doctor') as HTMLInputElement).value;
+        const specialty = (document.getElementById('specialty') as HTMLInputElement).value;
+        const date = (document.getElementById('date') as HTMLInputElement).value;
+        const location = (document.getElementById('location') as HTMLInputElement).value;
+        const extraInfo = (document.getElementById('extraInfo') as HTMLInputElement).value;
+
+        const appointmentData = {
+          doctor,
+          specialty,
+          date,
+          location,
+          extraInfo
+        };
+
+        return this.appointmentsService.createAppointment(appointmentData).toPromise();
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('Consulta criada com sucesso!');
+        this.getAllAppointments();
+        Swal.fire({
+          title: 'Sucesso',
+          text: 'Consulta criada com sucesso!',
+          icon: 'success',
+          confirmButtonColor: '#046B46',
+          confirmButtonText: 'Ok',
+        });
+      }
+    }).catch((error) => {
+      console.error('Erro ao criar consulta:', error);
+      Swal.fire({
+        title: 'Erro',
+        text: 'Erro ao criar consulta. Por favor, tente novamente.',
+        icon: 'error',
+        confirmButtonColor: '#046B46',
+        confirmButtonText: 'Ok',
+      });
+    });
   }
 }
